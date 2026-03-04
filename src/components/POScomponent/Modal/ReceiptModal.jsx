9f83be10bcef3f306bb811edba44514ffb9e6320
Rod Angelo <vendiolaangelo2@gmail.com>
2026-03-04 13:11:00 +0800

    Deployment Frontend

--- src/components/POScomponent/Modal/ReceiptModal.jsx
+++ src/components/POScomponent/Modal/ReceiptModal.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import API_BASE_URL from '../../../config/api';
 
 export default function ReceiptModal({
   transactionId,
--- src/components/POScomponent/Modal/TransactionDetailModal.jsx
+++ src/components/POScomponent/Modal/TransactionDetailModal.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import API_BASE_URL from '../../../config/api';
 
 export default function TransactionDetailModal({
   data,
